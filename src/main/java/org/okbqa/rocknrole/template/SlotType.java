9d8549ed6073a28053c5a4e30d7eee75bdd356e4
cunger <andrea.christina.unger@gmail.com>
2016-07-21 15:16:11 +0900

    backwards compatible slot types

--- src/main/java/org/okbqa/rocknrole/template/SlotType.java
+++ src/main/java/org/okbqa/rocknrole/template/SlotType.java
@@ -1,8 +1,5 @@
 package org.okbqa.rocknrole.template;
 
-import java.util.ArrayList;
-import java.util.List;
-
 /**
  *
  * @author cunger
@@ -19,15 +16,15 @@ public enum SlotType {
         
         String s;
         switch (this) {
-            case CLASS: s = "rdfs:Class"; break;
+            case CLASS: s = "rdf:Class"; break;
             case PROPERTY: s = "rdf:Property"; break;
             case OBJECTPROPERTY: s = "owl:ObjectProperty"; break;
             case DATAPROPERTY: s = "owl:DatatypeProperty"; break;
             case SORTAL: s = "<http://lodqa.org/vocabulary/sort_of>"; break;
-            case RESOURCE: s = "rdfs:Resource"; break;
-            case LITERAL: s = "rdfs:Literal"; break;
-            case RESOURCEorLITERAL: s = "rdfs:Resource|rdfs:Literal"; break;
-            case CLASSorRESOURCE: s = "rdfs:Class|rdfs:Resource"; break;
+            case RESOURCE: s = "rdf:Resource"; break;
+            case LITERAL: s = "rdf:Literal"; break;
+            case RESOURCEorLITERAL: s = "rdf:Resource|rdf:Literal"; break;
+            case CLASSorRESOURCE: s = "rdf:Class|rdf:Resource"; break;
             default: s = "UNSPEC";
         };
         return s;
